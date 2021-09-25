bfca4075c4e4a49188094b94a89292823433993e
petumazo <davidalvarezmazzo@gmail.com>
2021-09-25 00:17:57 -0500

    Adding a text input field and a submit button

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -5,9 +5,16 @@ class TodosList extends React.Component {
   render() {
     return (
       <ul>
-        {this.props.todos.map(todo => (
-          <TodoItem key={todo.id} todo={todo} />
-        ))}
+        {this.props.todos.map(
+          todo => (
+            <TodoItem 
+              key={todo.id} 
+              todo={todo} 
+              handleChangeProps={this.props.handleChangeProps}
+              deleteTodoProps={this.props.deleteTodoProps}
+            />
+          )
+        )}
       </ul>
     )
   }
